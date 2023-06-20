64b7d5f7421b15333d829ea376264b99a69610f0
14archana <apvkeezhana2000@gmail.com>
2023-06-20 23:11:41 +0530

    first

--- server/config/dBConfig.js
+++ server/config/dBConfig.js
@@ -1,14 +1,46 @@
- const mongoose = require('mongoose');
+//  const mongoose = require('mongoose');
+//  require('dotenv').config();
 
- mongoose.connect(process.env.mongo_url);
- const connection = mongoose.connection;
- connection.on('connected' ,()=>{
-    console.log('mongo DB connection established');
+//  mongoose.connect(process.env.mongo_url);
+//  const connection = mongoose.connection;
+//  connection.on('connected' ,()=>{
+//     console.log('mongo DB connection established');
 
- })
- connection.on('error',(err)=>{
-    console.log('mongoDB connection error');
- })
-
+//  })
+//  connection.on('error',(err)=>{
+//     console.log('mongoDB connection error');
+//  })
  
-   module.exports = connection;
+   // module.exports = connection;
+  
+
+
+
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// mongoose.connect(process.env.mongo_url, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(() => {
+//     console.log('MongoDB connection established');
+//   })
+//   .catch((error) => {
+//     console.error('MongoDB connection error:', error);
+//   });
+
+// const connection = mongoose.connection;
+// module.exports = connection;
+
+
+
+
+
+// mongoose.connect('mongodb+srv://krishna:sivam123@cluster0.czukufp.mongodb.net/')
+//     .then(() => {
+//     console.log("Database Connected");
+// })
+//     .catch(err => {
+//     console.log("Database Not Connected",err.message);
+// });
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const dbConfig = require('./config /dbConfig');
+
+const dBConfig = require('./config/dBConfig');
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Node/express js server started on port ${port}`));
 
